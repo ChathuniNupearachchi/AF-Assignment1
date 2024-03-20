132ba4946b6397ed6062d217bba79d98a5ec4b92
chathuni <nachathunidilmini@gmail.com>
2024-03-20 14:03:43 +0530

    Add authentication middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require('express')
 const router = express.Router();
 const {registerUser, loginUser, getMe} = require('../controllers/userController')
 const {protect} = require('../middleware/authMiddleware')
+const {studentAuthentication} = require('../middleware/authentication')
+
 
 router.post('/', registerUser)
 
 router.post('/login', loginUser)
 
-router.get('/me', protect,getMe)
+router.get('/me', protect,studentAuthentication,getMe)
 
 module.exports = router
